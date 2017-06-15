00c7a54367d686fbb98d980ecf99fc505a253b8c
Vedolin <Vedolin@users.noreply.github.com>
2017-06-15 18:04:56 -0300

    Initial setup

--- receitas.rb
+++ receitas.rb
@@ -1,45 +1,6 @@
-**** Grouping
-    rails generate scaffold Grouping name:string
 
-
-**** School
-    
-    rails generate scaffold School name:string address:text grouping:belongs_to
-
-
-
-**** Target 
-    belongs to school	
-
-	rails generate scaffold Target name:string school:belongs_to
-4 a 6 anos
-
-**** Kind
-    has meals
-
-    rails generate scaffold Kind name:string target:belongs_to
-
-
-**** Meal belongs to Kind
-	planejado
-	servido
-
-	rails generate scaffold Meal planned:text served:text shceduled_to:date target:belongs_to kind:belongs_to
-
-
-
-
-Arroz, feijao, abobrinha, frango desfiado, salada de alface
-Arroz, feijao, cenoura, frango desfiado, salada de couve
-
-
-Arroz, feijao, abobrinha, frango desfiado, salada de alface
-Arroz, lentilha, batata, peixe assado
-
-
-
-
-
+rails new alimentacao_escolar
+cd alimentacao_escolar
 
 rails destroy scaffold Group
 rails destroy scaffold Execution
