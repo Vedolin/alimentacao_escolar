b82d2d7b834295338647fca1f38c87a7cd91cd3e
Luis Vedolin <vedolin@yahoo.com>
2017-06-17 18:02:49 -0300

    Com a gem de fuzzy - mas que parece que deixaremos de usar

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,15 @@
 class Meal < ApplicationRecord
+  require 'fuzzystringmatch'
   belongs_to :school
   belongs_to :kind
   belongs_to :age
+
+def distance
+  my_distance = FuzzyStringMatch::JaroWinkler.create( :native )
+  my_distance.getDistance( self.planned.split(',').sort.join(','),self.served.split(',').sort.join(',') )
+end
+
+  # def levenshtein_distance
+  #   String::Similarity.levenshtein_distance(self.planned, self.served)
+  # end
 end
